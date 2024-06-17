52645214917c2893c298abca67084141d0175f36
mTayo <m.adinlewa@gmail.com>
2024-06-17 12:47:54 +0100

    outer border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-      <>      
+      <div className="">      
         <div className="stars" />
-        <Header />
+          <Header />
 
-      </>
+      </div>
 
   );
 }
