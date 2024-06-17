36885646ddc887f86106b3237da7976656c2bc20
mTayo <m.adinlewa@gmail.com>
2024-06-17 21:44:36 +0100

    feature cards wip

--- src/app/components/Features/index.tsx
+++ src/app/components/Features/index.tsx
@@ -5,15 +5,9 @@ export default function Features() {
     
     return (
             <>
-                {/* <Section /> */}
+          
                 <Section contentClass="" useContentBorder={false}>
-                    {/* <div className="mx-[120px] vertical-border-left  vertical-border-right flex justify-between items-center">
-                        {customerData.first_row.map((item, i) => (
-                            <div className={`py-10 px-12 ${i  < customerData.first_row.length? 'vertical-border-left' : ''}`} key={i}>
-                                <img src={item} alt={item} />
-                            </div>
-                        ))}
-                    </div> */}
+                    
                     <div className="pt-[121px] flex items-center">
                         <img src="/assets/feature-cards/card_1.svg" alt="Feature card"/>
                         <img src="/assets/feature-cards/card_2.svg" alt="Feature card"/>
