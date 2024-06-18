120721666045725f27f0fc2c4fa5e8501189cc76
mTayo <m.adinlewa@gmail.com>
2024-06-18 14:11:44 +0100

    avatar images

--- src/app/components/Features/index.tsx
+++ src/app/components/Features/index.tsx
@@ -8,28 +8,29 @@ export default function Features() {
             <>
           
                 <Section contentClass="" useContentBorder={false}>
-                    
-                    <div className="pt-[121px] flex justify-between items-center">
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_1.svg" alt="Feature card"/>
-                        </TiltCard>
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_2.svg" alt="Feature card"/>
-                        </TiltCard>
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_3.svg" alt="Feature card"/>
-                        </TiltCard>
-                    </div>
-                    <div className="mt-[-10px] pb-[121px] flex justify-between items-center">
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_4.svg" alt="Feature card"/>
-                        </TiltCard>
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_5.svg" alt="Feature card"/>
-                        </TiltCard>
-                        <TiltCard>
-                            <img src="/assets/feature-cards/card_6.svg" alt="Feature card"/>
-                        </TiltCard>
+                    <div className="features-section-container relative">
+                        <div className="pt-[121px] flex justify-between items-center">
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_1.svg" alt="Feature card"/>
+                            </TiltCard>
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_2.svg" alt="Feature card"/>
+                            </TiltCard>
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_3.svg" alt="Feature card"/>
+                            </TiltCard>
+                        </div>
+                        <div className="mt-[-10px] pb-[121px] flex justify-between items-center">
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_4.svg" alt="Feature card"/>
+                            </TiltCard>
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_5.svg" alt="Feature card"/>
+                            </TiltCard>
+                            <TiltCard>
+                                <img src="/assets/feature-cards/card_6.svg" alt="Feature card"/>
+                            </TiltCard>
+                        </div>
                     </div>
                 </Section>
                 <Section useContentBorder={false} />
